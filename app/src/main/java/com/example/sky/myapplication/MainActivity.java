66036f3b647563458f806d433dc63dde6157c211
xuzhiyong017 <912996973@qq.com>
2016-04-16 23:48:03 +0800

    提交

--- app/src/main/java/com/example/sky/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sky/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast.makeText(MainActivity.this, "ceshi", Toast.LENGTH_SHORT).show();
             }
         });
     }
